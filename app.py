22f47544e3f0ebb3ff3a575a007fc01890bbed68
Diego Almeyda <dalmeydacl@gmail.com>
2026-03-25 23:11:47 -0500

    conectando google sheets

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
+URL = "https://opensheet.elk.sh/1AZ61TA_TcghJIK5hn09dprJDwX_r_osktzgPS1NlZlc/Productos"
+
 @app.route("/")
 def inicio():
-    return render_template("index.html")
+    productos = requests.get(URL).json()
+    return render_template("index.html", productos=productos)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
